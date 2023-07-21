5655e29518b2f8f2ab67d8567a4bd16b0c868138
ZINE EDDINE <119061162+ZYNOCODES@users.noreply.github.com>
2023-07-21 03:21:29 +0100

    second commit

--- app/src/main/java/com/example/mycomposeapplication/MainActivity.kt
+++ app/src/main/java/com/example/mycomposeapplication/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.mycomposeapplication
 
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,43 +12,45 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.magnifier
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.mycomposeapplication.ui.theme.MyComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyComposeApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting()
+            val painter = painterResource(id = R.drawable.image)
+            val title = "it's algerien sahraa"
+            val description = "it's algerien sahraa"
+            Box(modifier = Modifier
+                .fillMaxWidth(1f)
+                .padding(20.dp),
+                contentAlignment = Alignment.TopCenter) {
+                Column(verticalArrangement = Arrangement.spacedBy(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                    ImageCard(painter = painter, Description = description, Title = title)
+                    ImageCard(painter = painter, Description = description, Title = title)
+                    ImageCard(painter = painter, Description = description, Title = title)
+
                 }
+                ImageCard(painter = painter, Description = description, Title = title)
             }
         }
     }
@@ -58,68 +59,70 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting() {
-    var name = remember {
-        mutableStateOf("")
-    }
-    Column(modifier = Modifier.padding(20.dp),
-    verticalArrangement = Arrangement.SpaceAround,
-    horizontalAlignment = Alignment.CenterHorizontally) {
-        Box() {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Image(painter = painterResource(id = R.drawable.img)
-                    , contentDescription = "BurgerIMG",
-                    modifier = Modifier.padding(bottom = 10.dp))
-                Text(text = "Hello Nada!",
-                    color = colorResource(id = R.color.Gray),
-                    fontSize = 25.sp,
-                    textDecoration = TextDecoration.Underline,
-                    letterSpacing = 3.sp,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(bottom = 10.dp))
-                Text(text = "Best place to write life stories and share your journey experiences",
-                    color = colorResource(id = R.color.Gray),
-                    fontSize = 15.sp,
-                    textDecoration = TextDecoration.Underline,
-                    fontWeight = FontWeight.Light,
-                    modifier = Modifier.padding(start = 20.dp, end = 20.dp))
-            }
-        }
 
-        TextField(value = name.value,
-            onValueChange = {name.value = it},
-            label = {Text(text = "Name")},)
-        Box() {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Box(modifier = Modifier
-                    .fillMaxWidth(1f)
-                    .height(5.dp)
-                    .background(color = colorResource(id = R.color.Gray))
-                    .padding(bottom = 10.dp)) {
-                }
-                Button(onClick = { /*TODO*/ },
-                    modifier = Modifier
-                        .background(
-                            color = colorResource(id = R.color.PrimaryColor),
-                            shape = CircleShape
+}
+@Composable
+fun ImageCard(
+    painter: Painter,
+    Description :String,
+    Title :String,
+    modifier: Modifier = Modifier
+){
+    Card(modifier = Modifier.fillMaxWidth(0.5f),
+        shape = RoundedCornerShape(15.dp),
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = Description ,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop)
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                colorResource(id = R.color.Lowblack)
+                            ),
+                            startY = 300f
                         )
-                        .width(200.dp)
-                        .height(47.dp)) {
-                    Text(text = "LOGIN",
-                        color = colorResource(id = R.color.white),
-                        fontSize = 15.sp,
-                        textDecoration = TextDecoration.Underline,
-                        letterSpacing = 2.sp,
-                        fontWeight = FontWeight.Bold)
-                }
+                    )) {
+
+
+            }
+            Box(contentAlignment = Alignment.BottomStart,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(start = 10.dp, bottom = 20.dp)) {
+                Text(text = Title,
+                    style = TextStyle(fontSize = 20.sp,
+                    fontFamily = FontFamily.Serif,
+                    color = colorResource(id = R.color.white))
+                )
             }
         }
     }
 }
-
-@Preview(showBackground = true)
+@Preview(showBackground = true,
+    showSystemUi = true)
 @Composable
 fun GreetingPreview() {
-    MyComposeApplicationTheme {
-        Greeting()
+    var painter = painterResource(id = R.drawable.image)
+    var title = "it's algerien sahraa"
+    var description = "it's algerien sahraa"
+    Box(modifier = Modifier
+        .fillMaxWidth(1f)
+        .padding(20.dp),
+        contentAlignment = Alignment.TopCenter) {
+        Column(verticalArrangement = Arrangement.spacedBy(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            ImageCard(painter = painter, Description = description, Title = title)
+            ImageCard(painter = painter, Description = description, Title = title)
+            ImageCard(painter = painter, Description = description, Title = title)
+
+        }
+        ImageCard(painter = painter, Description = description, Title = title)
     }
 }
